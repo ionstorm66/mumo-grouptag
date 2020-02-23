93a2733fcae2faedc6bf406fa3d5aad1db363e96
Timothy Hunter <ionstorm66@gmail.com>
2020-02-22 22:10:30 -0500

    Fix syntax error with if statement

--- grouptag.py
+++ grouptag.py
@@ -161,9 +161,9 @@ class grouptag(MumoModule):
         userstate = server.getState(int(user.session))
 
         if any(x.split(':')[1] in user.name for x in scfg.groupmap):
-            prefix = [x for x in scfg.groupmap if x.split(':')[1] in user.name][0].split(':')[1]
-            prefix = user.name.find(prefix)+len(prefix)
-            original_username = user.name[prefix:]
+          prefix = [x for x in scfg.groupmap if x.split(':')[1] in user.name][0].split(':')[1]
+          prefix = user.name.find(prefix)+len(prefix)
+          original_username = user.name[prefix:]
         else:
             userstate.name='%s' % (original_username)
 
